a4e5a67185b9afda3605d1c78543b8de514d9c16
MiyamotoAkira <yefo.akira@gmail.com>
2015-05-11 18:32:53 +0100

    Redo Romans because somehow I forgot to commit before

--- go/codekatas/Romans.go
+++ go/codekatas/Romans.go
@@ -2,7 +2,9 @@ package codekatas
 
 func ConvertOnesToNumerals(number int) string {
 	 switch number {
-	 	case 1:
+	 	case 0:
+			 return ""
+		case 1:
 			 return "I"
 		case 2:
 			 return "II"
@@ -21,4 +23,88 @@ func ConvertOnesToNumerals(number int) string {
 		case 9:
 			 return "IX"
 	 }
+
+	 return "NoNo"
+}
+
+func ConvertTensToNumerals(number int) string {
+	 switch number {
+	 	case 0:
+			 return ""
+	 	case 1:
+			 return "X"
+		case 2:
+			 return "XX"
+		case 3:
+			 return "XXX"
+		case 4:
+			 return "XL"
+		case 5:
+			 return "L"
+		case 6:
+			 return "LX"
+		case 7:
+			 return "LXX"
+		case 8:
+			 return "LXXX"
+		case 9:
+			 return "XC"
+	 }
+
+	 return "NoNo"
+}
+
+func ConvertHundredsToNumerals(number int) string {
+	 switch number {
+	 	case 0:
+			 return ""
+	 	case 1:
+			 return "C"
+		case 2:
+			 return "CC"
+		case 3:
+			 return "CCC"
+		case 4:
+			 return "CD"
+		case 5:
+			 return "D"
+		case 6:
+			 return "DC"
+		case 7:
+			 return "DCC"
+		case 8:
+			 return "DCCC"
+		case 9:
+			 return "CM"
+	 }
+
+	 return "NoNo"
+}
+
+
+func ConvertThousandsToNumerals(number int) string {
+	 switch number {
+	 	case 0:
+			 return ""
+	 	case 1:
+			 return "M"
+		case 2:
+			 return "MM"
+		case 3:
+			 return "MMM"
+	 }
+
+	 return "NoNo"
+}
+
+func ConvertToRomans(number int) string {
+	 ones := number % 10
+	 tens := number / 10 % 10
+	 hundreds := number / 100 % 10
+	 thousands := number / 1000 % 10
+	 onesNumeral := ConvertOnesToNumerals(ones)
+	 tensNumeral := ConvertTensToNumerals(tens)
+	 hundredsNumeral := ConvertHundredsToNumerals(hundreds)
+	 thousandsNumeral := ConvertThousandsToNumerals(thousands)
+	 return thousandsNumeral + hundredsNumeral + tensNumeral + onesNumeral
 }
