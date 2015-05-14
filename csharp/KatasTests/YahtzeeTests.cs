e66ce862a5b0526e0632ae5a6609962b7ccfabc7
MiyamotoAkira <yefo.akira@gmail.com>
2015-05-14 19:07:05 +0100

    Simple numbers on Yahtzee are now calculated correctly

---
+++ csharp/KatasTests/YahtzeeTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using FluentAssertions;
+using katasAlgorithms;
+
+namespace KatasTests
+{        
+    [TestFixture]
+    [Category("Unit")]
+    public class YahtzeeTests
+    {
+        [Test]
+        public void ConsolidateDice()
+        {
+            Yahtzee yahtzee = new Yahtzee();
+            var result = yahtzee.ConsolidateDice(new List<int> { 1, 4, 4, 3, 3 });
+            result [1].Should().Be(1);
+            result [4].Should().Be(2);
+            result [3].Should().Be(2);
+        }
+
+        [Test]
+        public void CalculateRoll()
+        {
+            Yahtzee yahtzee = new Yahtzee();
+            foreach (TestCase testCase in SetTestCases())
+            {
+                var result = yahtzee.CalculateRoll(testCase.Dice, testCase.OptionChosen);
+                result.Should().Be(
+                    testCase.Expected, 
+                    "For Dice {0}, on option {1} the expected result is {2}", 
+                    testCase.Dice.ToString(), 
+                    testCase.OptionChosen, 
+                    testCase.Expected);
+            }
+        }
+
+        public IEnumerable<TestCase> SetTestCases()
+        {
+            List<TestCase> testCases = new List<TestCase>();
+            testCases.Add(new TestCase(new List<int>{ 1, 3, 2, 2, 5 }, Options.ONES, 1));
+            testCases.Add(new TestCase(new List<int>{ 1, 3, 2, 2, 5 }, Options.TWOS, 4));
+            testCases.Add(new TestCase(new List<int>{ 3, 3, 3, 3, 5 }, Options.THREES, 12));
+            testCases.Add(new TestCase(new List<int>{ 4, 3, 4, 3, 5 }, Options.FOURS, 8));
+            testCases.Add(new TestCase(new List<int>{ 4, 3, 4, 3, 5 }, Options.FIVES, 5));
+            testCases.Add(new TestCase(new List<int>{ 4, 3, 4, 3, 5 }, Options.SIXES, 0));
+            testCases.Add(new TestCase(new List<int>{ 6, 6, 6, 3, 5 }, Options.SIXES, 18));
+            return testCases;
+        }
+    }
+
+    public struct TestCase
+    {
+        public List<int> Dice;
+        public Options OptionChosen;
+        public int Expected;
+
+        public TestCase(List<int> dice, Options option, int expected)
+        {
+            Dice = dice;
+            OptionChosen = option;
+            Expected = expected;
+        }
+    }
+}
+
---
+++ csharp/katasAlgorithms/Yahtzee.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace katasAlgorithms
+{
+    public enum Options
+    {
+        ONES = 1,
+        TWOS,
+        THREES,
+        FOURS,
+        FIVES,
+        SIXES,
+        PAIR,
+        TWOPAIR,
+        THREEKIND,
+        FOURKIND,
+        SMALL,
+        LARGE,
+        YAHTZEE
+    }
+
+    public class Yahtzee
+    {
+        public int CalculateRoll (IEnumerable<int> dice, Options option)
+        {
+            switch (option)
+            {
+            case Options.ONES:
+            case Options.TWOS:
+            case Options.THREES:
+            case Options.FOURS:
+            case Options.FIVES:
+            case Options.SIXES:
+                return CalculateSimpleNumbers(dice, (int)option);
+            }
+
+            return 0;
+        }
+
+        public int CalculateSimpleNumbers(IEnumerable<int> dice, int die)
+        {
+            var consolidated = ConsolidateDice(dice);
+            if (consolidated.ContainsKey(die))
+            {
+                return die * consolidated [die];
+            }
+
+            return 0;
+        }
+
+        public Dictionary<int, int> ConsolidateDice(IEnumerable<int> dice)
+        {
+            Dictionary<int, int> consolidated = new Dictionary<int, int>();
+
+            foreach (int die in dice)
+            {
+                if (!consolidated.ContainsKey(die))
+                {
+                    consolidated.Add(die, 0);
+                }
+
+                consolidated [die] += 1;
+            }
+
+            return consolidated;
+        }
+    }
+}
+
