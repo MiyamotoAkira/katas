a4f6a3b70982ca0a9689b1cceb401b02c76766c2
MiyamotoAkira <yefo.akira@gmail.com>
2015-05-14 09:00:59 +0100

    Created FizzBuzz on c#

---
+++ csharp/KatasTests/FizzBuzzTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+using katasAlgorithms;
+using FluentAssertions;
+
+namespace KatasTests
+{
+    [TestFixture]
+    [Category("Unit")]
+    public class FizzBuzzTests
+    {
+        public FizzBuzzTests ()
+        {
+        }
+
+        [Test]
+        [TestCase(1, "1")]
+        [TestCase(3, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(6, "Fizz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        public void SingleNumber_ANumberIsPassed_ExpectedLiteral(int number, string literal)
+        {
+            var fizzBuzz = new FizzBuzz ();
+            var result = fizzBuzz.ConvertSingle(number);
+            result.Should().Be(literal);
+        }
+
+        [Test]
+        public void Run() 
+        {
+            var fizzBuzz = new FizzBuzz ();
+            var result = fizzBuzz.Run(15);
+            result.Should().Be("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz");
+        }
+    }
+}
+
---
+++ csharp/katasAlgorithms/FizzBuzz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace katasAlgorithms
+{
+    public class FizzBuzz
+    {
+        public string ConvertSingle(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+
+            return number.ToString();
+        }
+
+        public string Run(int numberOfIterations)
+        {
+            List<string> results = new List<string>();
+            for (int counter = 1; counter <= numberOfIterations; counter++)
+            {
+                results.Add(ConvertSingle(counter));
+            }
+
+            return String.Join(" ", results);
+        }
+    }
+}
+
