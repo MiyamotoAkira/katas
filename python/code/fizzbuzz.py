6222ad12a2264dd96443fdc65a846ae7a42a621c
MiyamotoAkira <yefo.akira@gmail.com>
2015-05-16 11:29:51 +0100

    FizzBuzz basic completed

--- python/code/fizzbuzz.py
+++ python/code/fizzbuzz.py
@@ -1,2 +1,21 @@
 def single_one(number):
-    return number
+    divBy3 = number % 3 == 0
+    divBy5 = number % 5 == 0
+
+    if (divBy3 and divBy5):
+        return "FizzBuzz"
+    
+    if (divBy3):
+        return "Fizz"
+
+    if (divBy5):
+        return "Buzz"
+    
+    return str(number)
+
+def fizz_buzz(limit):
+    literals = []
+    for number in range(1, limit+1):
+        literals.append(single_one(number))
+
+    return ' '.join(literals)
--- python/katatest/test_fizzbuzz.py
+++ python/katatest/test_fizzbuzz.py
@@ -1,8 +1,23 @@
 import unittest
+from nose_parameterized import parameterized
 from code import fizzbuzz
 
 class FizzBuzzTest(unittest.TestCase):
-    def test_single_one(self):
-        result = fizzbuzz.single_one(1)
-        self.assertEqual(result, "1")
+    @parameterized.expand([
+        (1, "1"),
+        (3, "Fizz"),
+        (6, "Fizz"),
+        (5, "Buzz"),
+        (10, "Buzz"),
+        (15, "FizzBuzz")
+        ])
+    def test_single_one(self, number, expected):
+        result = fizzbuzz.single_one(number)
+        self.assertEqual(result, expected)
+
+
+    def test_fizz_buzz(self):
+        result = fizzbuzz.fizz_buzz(20)
+        self.assertEqual(result, "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz")
+        
             
